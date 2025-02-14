24023e645c8675148db1cf1fcccb25c1de8bdbaa
Anubhav Nair <anubhavnair023@gmail.com>
2025-02-14 08:46:11 +0530

    database connection

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import dotenv from 'dotenv';    
 dotenv.config();
 import express from 'express';
+import connectToDb from './db/db.js';
+import cors from 'cors';
 const app  = express();
 
+app.use(cors());    
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 
+await connectToDb();
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
