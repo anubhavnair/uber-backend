b9da63eb02e4e19d5ce1dd6479f094dead00aba0
Anubhav Nair <anubhavnair023@gmail.com>
2025-02-18 13:15:34 +0530

    authorization of simple user has been done now have to work for captain

--- app.js
+++ app.js
@@ -8,17 +8,23 @@ import userRoutes from './routes/user.routes.js';
 
 const app  = express();
 
+// middle wares 
 app.use(cors());    
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
 
 
+// database connection
 await connectToDb();
+
+// route middlewares 
 app.use('/api/users',userRoutes);
+
+// sample server check 
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
 
-
+// exporting the app to use the server.js
 export default app;
