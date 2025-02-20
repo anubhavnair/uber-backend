385655d80daf1e875afb305332859a2d67a19edd
Anubhav Nair <anubhavnair023@gmail.com>
2025-02-20 13:41:04 +0530

    create login signup forms for user and captain both

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
 import dotenv from 'dotenv';    
 dotenv.config();
 import express from 'express';
-import cookieParser from 'cookie-parser';
-import connectToDb from './db/db.js';
-import cors from 'cors';
-import userRoutes from './routes/user.routes.js';
-import capRoutes from './routes/captain.routes.js';
-
 const app  = express();
 
-// middle wares 
-app.use(cors());    
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser())
 
 
-// database connection
-await connectToDb();
-
-// route middlewares 
-app.use('/api/users',userRoutes);
-app.use('/api/captain',capRoutes);
-
-// sample server check 
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
 
-// exporting the app to use the server.js
+
 export default app;
